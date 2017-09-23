89604450de502ce19e0d33c4d9a51b07559ca62b
tns <than.karidis@gmail.com>
2017-09-23 17:53:08 +0300

    model changed

--- src/model/Owners.java
+++ src/model/Owners.java
@@ -1,4 +1,32 @@
 package model;
 
 public class Owners {
+
+    private int ownerId;
+    private String firstName;
+    private String lastName;
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getFirstName(String firstName){
+        return firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getLastName(String lastName){
+        return lastName;
+    }
 }
--- src/model/Vehicles.java
+++ src/model/Vehicles.java
@@ -1,4 +1,32 @@
 package model;
 
 public class Vehicles {
+
+    private String plate;
+    private String activationDate;
+    private int ownerId;
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getActivationDate() {
+        return activationDate;
+    }
+
+    public void setActivationDate(String activationDate) {
+        this.activationDate = activationDate;
+    }
 }
